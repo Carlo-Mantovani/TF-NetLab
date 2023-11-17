d73c5af21fdb00707508849ad8a8e0f7bd287c59
Carlo Mantovani <105457981+Carlo-Mantovani@users.noreply.github.com>
2023-11-17 18:15:30 -0300

    fix: automation process with 2 docker networks

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 # Docker compose build
-sudo docker compose build
+sudo docker compose up -d --build
 
 # Allow traffic between containers in different networks
 sudo iptables -I DOCKER-ISOLATION-STAGE-2 -o br0 -i br1 -j ACCEPT
