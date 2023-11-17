8aa9c32aa36f7cc39b214cc52ab5d3baa07b18fe
Carlo-Mantovani <c.mantovani@edu.pucrs.br>
2023-11-17 11:11:33 -0300

    feat: list package types and addresses

--- receive.py
+++ receive.py
@@ -2,46 +2,81 @@ import socket
 import struct
 import binascii
 import os
-import scapy.layers.all as spy
+import scapy.all as spy
 
 
 
 network_interface = 'br0'
 
-def parse_ethernet_header(data):
-    dest_mac, src_mac, eth_proto = struct.unpack('! 6s 6s H', data[:14])
-    return get_mac_address(dest_mac), get_mac_address(src_mac), socket.htons(eth_proto), data[14:]
+def get_packet_type(packet):
+    if packet.haslayer(spy.ARP):
+        if packet[spy.ARP].op == 1:
+            return 'ARP Request'
+        elif packet[spy.ARP].op == 2:
+            return 'ARP Reply'
+    elif packet.haslayer(spy.UDP):
+        return 'UDP'
+    elif packet.haslayer(spy.TCP):
+        return 'TCP'
+    elif packet.haslayer(spy.ICMP):
+        return 'ICMP'
+    else:
+        return 'UNKNOWN'
 
-def get_mac_address(bytes_address):
-    bytes_str = map('{:02x}'.format, bytes_address)
-    return ':'.join(bytes_str).upper()
+def get_packet_MAC_addresses(packet):
+    src_mac = packet.src
+    dst_mac = packet.dst
 
-def parse_ip_header(data):
-    version_ihl, dscp_ecn, total_length, identification, flags_fragOffset, ttl_protocol, header_checksum, src_ip, dest_ip = struct.unpack('! B B H H H B B H 4s 4s', data[:20])
-    version = version_ihl >> 4
-    ihl = (version_ihl & 0xF) * 4
-    return version, ihl, dscp_ecn, total_length, identification, flags_fragOffset, ttl_protocol, header_checksum, socket.inet_ntoa(src_ip), socket.inet_ntoa(dest_ip), data[ihl:]
+    return src_mac, dst_mac   
 
-def parse_tcp_header(data):
-    src_port, dest_port, sequence, acknowledgment, offset_reserved_flags = struct.unpack('! H H L L H', data[:14])
-    offset = (offset_reserved_flags >> 12) * 4
-    flags = offset_reserved_flags & 0x1FF
-    return src_port, dest_port, sequence, acknowledgment, offset, flags, data[offset:]
+def get_packet_IP_addresses(packet):
+    src_ip = packet[spy.IP].src
+    dst_ip = packet[spy.IP].dst
 
-def parse_udp_header(data):
-    src_port, dest_port, length = struct.unpack('! H H H', data[:6])
-    return src_port, dest_port, length, data[8:]
+    return src_ip, dst_ip
+
+def print_packet_info(packet, packet_type, src_mac, dst_mac, src_ip, dst_ip):
+    if packet_type == 'ARP Request' or packet_type == 'ARP Reply':
+        print (f'\nPacket Type: {packet_type}')
+        #print arp message
+        print(f'{packet.getlayer(spy.ARP)}')
+  
+    elif packet_type == 'ICMP':
+        print (f'\nPacket Type: {packet_type}')
+        #print icmp message
+        print(f'{packet.getlayer(spy.ICMP)}')
+    else:
+        print (f'\nPacket Type: {packet_type}')
+        print (f'{packet.summary()}')
+    print (f'Source MAC: {src_mac}, Destination MAC: {dst_mac}')
+    if src_ip != '' and dst_ip != '':
+        print (f'Source IP: {src_ip}, Destination IP: {dst_ip}')
+
+def get_packet_info(packet):
+    packet_type = get_packet_type(packet)
+    src_mac, dst_mac = get_packet_MAC_addresses(packet)
+    if packet.haslayer(spy.IP):
+        src_ip, dst_ip = get_packet_IP_addresses(packet)
+    else:
+        src_ip = ''
+        dst_ip = ''
+    print_packet_info(packet, packet_type, src_mac, dst_mac, src_ip, dst_ip)
+    return packet_type, src_mac, dst_mac, src_ip, dst_ip
+
+#def get_packet_layers(packet):
+#    counter = 0
+#    while True:
+#        layer = packet.getlayer(counter)
+#        if layer is None:
+#            break
+#
+#        yield layer
+#        counter += 1
 
 
-def get_packet_layers(packet):
-    counter = 0
-    while True:
-        layer = packet.getlayer(counter)
-        if layer is None:
-            break
 
-        yield layer
-        counter += 1
+
+
 
 def main():
    # print(os.system("ifconfig"))
@@ -50,43 +85,15 @@ def main():
     s.bind((network_interface, 0))
     
     while True:
+        print ('Waiting for packet')
         raw_data, addr = s.recvfrom(65536)
         #print (raw_data)
         #use scapy to parse the packet
         packet = spy.Ether(raw_data)
-        # print protocol, source and destination MAC addresses
-        #print("Protocol: ", packet.type)
-
-      #  print("Source: ", packet.src)
-        #print("Destination: ", packet.dst)
-        for layer in get_packet_layers(packet):
-            print(layer)
-       # dest_mac, src_mac, eth_proto, data = parse_ethernet_header(raw_data)
-        #print(f'\nEthernet Header:\nDestination MAC: {dest_mac}, Source MAC: {src_mac}, Protocol: {eth_proto}')
-       # print(f' \nData: {data}')
-
-      #  if eth_proto == 8:  # IPv4
-           # version, ihl, dscp_ecn, total_length, identification, flags_fragOffset, ttl_protocol, header_checksum, src_ip, dest_ip, data = parse_ip_header(data)
-
-           # if ttl_protocol == 6:  # TCP
-               # src_port, dest_port, sequence, acknowledgment, offset, flags, data = parse_tcp_header(data)
-
-                # Aqui você pode adicionar lógica para verificar pacotes TCP e contar a quantidade de pacotes TCP.
-
-           # elif ttl_protocol == 17:  # UDP
-                #src_port, dest_port, length, data = parse_udp_header(data)
-
-                # Aqui você pode adicionar lógica para verificar pacotes UDP e contar a quantidade de pacotes UDP.
-
-        # Adicione lógica para outros protocolos, se necessário.
-        # print all information
-        #print('\nEthernet Header:')
-        #print('Destination MAC: {}, Source MAC: {}, Protocol: {}'.format(dest_mac, src_mac, eth_proto))
-       # print('\nIP Header:')
-        #print('Version: {}, IHL: {}, DSCP/ECN: {}, Total Length: {}, Identification: {}, Flags/Frag Offset: {}, TTL: {}, Protocol: {}, Header Checksum: {}, Source IP: {}, Destination IP: {}'.format(version, ihl, dscp_ecn, total_length, identification, flags_fragOffset, ttl_protocol, header_checksum, src_ip, dest_ip))
-       # print('\nTCP Header:')
-       #print('Source Port: {}, Destination Port: {}, Sequence: {}, Acknowledgment: {}, Offset: {}, Flags: {}'.format(src_port, dest_port, sequence, acknowledgment, offset, flags))
-       # print('\nData:')
-       # print(data)
+        packet_type, src_mac, dst_mac, src_ip, dst_ip = get_packet_info(packet)
+        
+        
+        
+     
 if __name__ == "__main__":
     main()
--- send.py
+++ send.py
@@ -7,7 +7,7 @@ def main():
         # send when user press enter
         print('Sending message')
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.sendto('Hello World'.encode(), ('172.20.0.1', 8001))
+        s.sendto('Hello World'.encode(), ('172.20.0.4', 8081))
         time.sleep(10)
 
 if __name__ == '__main__':
